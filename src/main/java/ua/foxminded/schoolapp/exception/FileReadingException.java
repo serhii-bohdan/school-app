b957656f2fea35d4691f3b6dc6beb487018e04d5
Serhii Bohdan <serhii.bohdan99@gmail.com>
2023-05-04 01:50:21 +0300

    Added serialVersionUID field to FileReadingException

--- src/main/java/ua/foxminded/schoolapp/exception/FileReadingException.java
+++ src/main/java/ua/foxminded/schoolapp/exception/FileReadingException.java
@@ -2,7 +2,7 @@ package ua.foxminded.schoolapp.exception;
 
 public class FileReadingException extends RuntimeException {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 9139393289623292368L;
 
     public FileReadingException(String message) {
         super(message);
