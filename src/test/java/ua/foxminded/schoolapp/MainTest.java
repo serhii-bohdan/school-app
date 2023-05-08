48f9cf83c38aa4165b88b47388f573149bbd2c99
Serhii Bohdan <serhii.bohdan99@gmail.com>
2023-05-08 21:49:40 +0300

    Created several test classes

---
+++ src/test/java/ua/foxminded/schoolapp/MainTest.java
@@ -0,0 +1,13 @@
+package ua.foxminded.schoolapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ src/test/java/ua/foxminded/schoolapp/datasetup/ReaderImplTest.java
@@ -0,0 +1,13 @@
+package ua.foxminded.schoolapp.datasetup;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class ReaderImplTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
