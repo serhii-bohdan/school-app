605cc955af14d05eb2fcfe25c820c61ecbc6845c
Serhii Bohdan <serhii.bohdan99@gmail.com>
2023-05-31 18:16:49 +0300

    Added the \r symbol in text from ReaderImplTest test case

--- src/test/java/ua/foxminded/schoolapp/datasetup/impl/ReaderImplTest.java
+++ src/test/java/ua/foxminded/schoolapp/datasetup/impl/ReaderImplTest.java
@@ -93,9 +93,9 @@ class ReaderImplTest {
     @Test
     void readAllFileToString_shouldFileContentsAsString_whenFileIsSQLQuery() {
         String expectedFileContents = """
-                SELECT students.first_name, courses.course_name
-                FROM students
-                INNER JOIN students_courses ON students.student_id = students_courses.fk_student_id
+                SELECT students.first_name, courses.course_name\r
+                FROM students\r
+                INNER JOIN students_courses ON students.student_id = students_courses.fk_student_id\r
                 INNER JOIN courses ON courses.course_id = students_courses.fk_course_id;""";
 
         String actualFileLinesList = reader.readAllFileToString("readingtest/query_test.sql");
--- src/test/resources/readingtest/query_test.sql
+++ src/test/resources/readingtest/query_test.sql
@@ -1,4 +1,4 @@
 SELECT students.first_name, courses.course_name
 FROM students
 INNER JOIN students_courses ON students.student_id = students_courses.fk_student_id
-INNER JOIN courses ON courses.course_id = students_courses.fk_course_id;            
+INNER JOIN courses ON courses.course_id = students_courses.fk_course_id;
