e41c73cedd596a28a64e59a020c5a1dacbb0bb04
Serhii Bohdan <serhii.bohdan99@gmail.com>
2023-06-11 17:46:44 +0300

    Added tests to remaining methods from ConsoleView class

--- src/test/java/ua/foxminded/schoolapp/cli/ConsoleViewTest.java
+++ src/test/java/ua/foxminded/schoolapp/cli/ConsoleViewTest.java
@@ -7,10 +7,14 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ua.foxminded.schoolapp.model.Group;
+import ua.foxminded.schoolapp.model.Student;
 
 class ConsoleViewTest {
 
@@ -149,6 +153,198 @@ class ConsoleViewTest {
         String actualReturnedWord = view.getWordFromUser("Message");
 
         assertEquals(expectedReturnedWord, actualReturnedWord);
+    }
+
+    @Test
+    void getConfirmationFromUserAboutDeletingStudent_shouldNullPointerException_whenStudnetIsNull() {
+        view = new ConsoleView(scannerMock);
+
+        assertThrows(NullPointerException.class, () -> view.getConfirmationFromUserAboutDeletingStudent(null));
+    }
+
+    @Test
+    void getConfirmationFromUserAboutDeletingStudent_shouldConfirmationMessageWithNullAndNull_whenStudentFieldsNotInitialized() {
+        view = new ConsoleView(scannerMock);
+        Student student = new Student();
+        String expectedConfirmationMessage = "Are you sure you want to delete a student null null?\n"
+                                           + "Please confirm your actions (enter Y or N): ";
+        when(scannerMock.next()).thenReturn("Y");
+
+        view.getConfirmationFromUserAboutDeletingStudent(student);
+        String actualConfirmationMessage = baos.toString();
+
+        assertEquals(expectedConfirmationMessage, actualConfirmationMessage);
+    }
+
+    @Test
+    void getConfirmationFromUserAboutDeletingStudent_shouldConfirmationMessageWithStudentFirstNameAndLastName_whenStudentHasInitializedFfirstAndLastNamesFields() {
+        view = new ConsoleView(scannerMock);
+        Student student = new Student("FirstName", "LastName", 1);
+        String expectedConfirmationMessage = "Are you sure you want to delete a student FirstName LastName?\n"
+                                           + "Please confirm your actions (enter Y or N): ";
+        when(scannerMock.next()).thenReturn("Y");
+
+        view.getConfirmationFromUserAboutDeletingStudent(student);
+        String actualConfirmationMessage = baos.toString();
+
+        assertEquals(expectedConfirmationMessage, actualConfirmationMessage);
+    }
+
+    @Test
+    void getConfirmationFromUserAboutDeletingStudent_shouldReturnedOneCharacterConfirmation_whenScannerMockReturnOneCharacter() {
+        view = new ConsoleView(scannerMock);
+        Student student = new Student("FirstName", "LastName", 1);
+        String expectedConfirmationFromUser = "Y";
+        when(scannerMock.next()).thenReturn(expectedConfirmationFromUser);
+
+        String actualConfirmationFromUser = view.getConfirmationFromUserAboutDeletingStudent(student);
+
+        assertEquals(expectedConfirmationFromUser, actualConfirmationFromUser);
+    }
+
+    @Test
+    void getConfirmationFromUserAboutDeletingStudent_shouldReturnedNullConfirmation_whenScannerMockReturnNull() {
+        view = new ConsoleView(scannerMock);
+        Student student = new Student("FirstName", "LastName", 1);
+        String expectedConfirmationFromUser = null;
+        when(scannerMock.next()).thenReturn(expectedConfirmationFromUser);
+
+        String actualConfirmationFromUser = view.getConfirmationFromUserAboutDeletingStudent(student);
+
+        assertEquals(expectedConfirmationFromUser, actualConfirmationFromUser);
+    }
+
+    @Test
+    void displayGroups_shouldNullPointerException_whenGroupsListIsNull() {
+        view = new ConsoleView(scannerMock);
+
+        assertThrows(NullPointerException.class, () -> view.displayGroups(null));
+    }
+
+    @Test
+    void displayGroups_shouldNullPointerException_whenGroupsListContainsNull() {
+        view = new ConsoleView(scannerMock);
+        List<Group> groups = new ArrayList<>();
+        groups.add(null);
+
+        assertThrows(NullPointerException.class, () -> view.displayGroups(groups));
+    }
+
+    @Test
+    void displayGroups_shouldMessageThatGroupsListIsEmpty_whenGroupsListEmpty() {
+        view = new ConsoleView(scannerMock);
+        String expectedOutputMessage = "The list of groups is empty.\n"
+                                     + "\r\n";
+        List<Group> groups = new ArrayList<>();
+
+        view.displayGroups(groups);
+        String actualOutputMessage = baos.toString();
+
+        assertEquals(expectedOutputMessage, actualOutputMessage);
+    }
+
+    @Test
+    void displayGroups_shouldDisplayedGroupsList_whenGroupsListContainsThreeGroupsWithCorrectNames() {
+        view = new ConsoleView(scannerMock);
+        List<Group> groups = new ArrayList<>();
+        groups.add(new Group("JR-84"));
+        groups.add(new Group("QL-03"));
+        groups.add(new Group("VA-72"));
+        String expectedDisplayedGroups = "Groups:\n"
+                                       + "       JR-84\n"
+                                       + "       QL-03\n"
+                                       + "       VA-72\n"
+                                       + "\r\n";
+
+        view.displayGroups(groups);
+        String actualDisplayedGroups = baos.toString();
+
+        assertEquals(expectedDisplayedGroups, actualDisplayedGroups);
+    }
+
+    @Test
+    void displayGroups_shouldDisplayedGroupsList_whenGroupsListContainsThreeGroupsWithNotInitializedNames() {
+        view = new ConsoleView(scannerMock);
+        List<Group> groups = new ArrayList<>();
+        groups.add(new Group());
+        groups.add(new Group());
+        groups.add(new Group());
+        String expectedDisplayedGroups = "Groups:\n"
+                                       + "       null\n"
+                                       + "       null\n"
+                                       + "       null\n"
+                                       + "\r\n";
+
+        view.displayGroups(groups);
+        String actualDisplayedGroups = baos.toString();
+
+        assertEquals(expectedDisplayedGroups, actualDisplayedGroups);
+    }
+
+    @Test
+    void displayStudents_shouldNullPointerException_whenStudentsListIsNull() {
+        view = new ConsoleView(scannerMock);
+
+        assertThrows(NullPointerException.class, () -> view.displayStudents(null));
+    }
+
+    @Test
+    void displayStudents_shouldNullPointerException_whenStudentsListContainsNull() {
+        view = new ConsoleView(scannerMock);
+        List<Student> students = new ArrayList<>();
+        students.add(null);
+
+        assertThrows(NullPointerException.class, () -> view.displayStudents(students));
+    }
+
+    @Test
+    void displayStudents_shouldMessageThatStudentsListIsEmpty_whenStudentsListEmpty() {
+        view = new ConsoleView(scannerMock);
+        String expectedOutputMessage = "The list of students is empty.\r\n";
+        List<Student> students = new ArrayList<>();
+
+        view.displayStudents(students);
+        String actualOutputMessage = baos.toString();
+
+        assertEquals(expectedOutputMessage, actualOutputMessage);
+    }
+
+    @Test
+    void displayStudents_shouldDisplayedStudentsList_whenStudentsListContainsThreeStudentsWithCorrectNames() {
+        view = new ConsoleView(scannerMock);
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("FirstName_1", "LastName_1", 1));
+        students.add(new Student("FirstName_2", "LastName_2", 1));
+        students.add(new Student("FirstName_3", "LastName_3", 1));
+        String expectedDisplayedStudents = "Students:\n"
+                                         + "         FirstName_1 LastName_1\n"
+                                         + "         FirstName_2 LastName_2\n"
+                                         + "         FirstName_3 LastName_3\n"
+                                         + "\r\n";
+
+        view.displayStudents(students);
+        String actualDisplayedStudents = baos.toString();
+
+        assertEquals(expectedDisplayedStudents, actualDisplayedStudents);
+    }
+
+    @Test
+    void displayStudents_shouldDisplayedStudentsList_whenStudentsListContainsThreeStudentsWithNotInitializedNames() {
+        view = new ConsoleView(scannerMock);
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(null, null, 1));
+        students.add(new Student(null, null, 1));
+        students.add(new Student(null, null, 1));
+        String expectedDisplayedStudents = "Students:\n"
+                                         + "         null null\n"
+                                         + "         null null\n"
+                                         + "         null null\n"
+                                         + "\r\n";
+
+        view.displayStudents(students);
+        String actualDisplayedStudents = baos.toString();
+
+        assertEquals(expectedDisplayedStudents, actualDisplayedStudents);
     }
 
     @AfterEach
